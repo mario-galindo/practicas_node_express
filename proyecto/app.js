3adc3c7aceb33a0c9cc31ef85ada32c4aa2e47b8
Mario Galindo <mgalindo9410@gmail.com>
2016-05-14 23:16:06 -0600

    conexion con mongodb usando node.js

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,10 +1,27 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
+mongoose.connect("mongodb://localhost/fotos");
+
+var userSchemaJSON = {
+  email:String,
+  password:String
+};
+
+var user_schema = new Schema(userSchemaJSON);
+var User = mongoose.model("User",user_schema);
+
+//Para Servir rutas estaticas
 app.use("/public",express.static('public'));
+
+//Para manejar los datos enviado desde la vista por HTTP
 app.use(bodyParser.json());     //Para peticiones aplication/json
 app.use(bodyParser.urlencoded({extended:true}));
+
+//Motor de Vistas
 app.set("view engine","jade");
 
 app.get("/",function(req,res){
@@ -12,13 +29,21 @@ app.get("/",function(req,res){
 })
 
 app.get("/login",function(req,res){
-  res.render("login");
+
+  User.find(function(err,doc){
+    console.log(doc);
+    res.render("login");
+  });
+
 })
 
 app.post("/users",function(req,res){
-  console.log("Contraseña: " + req.body.password)
-  console.log("Contraseña: " + req.body.email)
-  res.send("Recibimos tus datos");
+  var user = new User({email:req.body.email, password:req.body.password});
+
+  user.save(function(){
+    res.send("Guardamos tus datos!!!");
+  });
+
 })
 
 app.listen(3000);
